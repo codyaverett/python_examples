89407a25a041e677b9d4fcefae35b9ba166ab026
Cody Averett <codyaverett@gmail.com>
2022-09-28 11:28:20 -0500

    add hackerrank mini-max sum answer

---
+++ hackerrank/mini-max_sum/main.py
@@ -0,0 +1,25 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'miniMaxSum' function below.
+#
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def miniMaxSum(arr):
+    arr.sort()
+    min_sum = sum(arr[:4])
+    max_sum = sum(arr[1:])
+    print(min_sum, max_sum)
+
+if __name__ == '__main__':
+
+    arr = list(map(int, input().rstrip().split()))
+
+    miniMaxSum(arr)
