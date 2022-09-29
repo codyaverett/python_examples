405dddbe7d30b1439bd45ddc42e2295d501d3e27
Cody Averett <codyaverett@gmail.com>
2022-09-29 05:49:02 -0500

    WIP: add hackerrank grid challenge starter

---
+++ hackerrank/_wip_grid_challenge/main.py
@@ -0,0 +1,40 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'gridChallenge' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts STRING_ARRAY grid as parameter.
+#
+
+
+def gridChallenge(grid):
+    # Write your code here
+    pass
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        n = int(input().strip())
+
+        grid = []
+
+        for _ in range(n):
+            grid_item = input()
+            grid.append(grid_item)
+
+        result = gridChallenge(grid)
+
+        fptr.write(result + '\n')
+
+    fptr.close()
